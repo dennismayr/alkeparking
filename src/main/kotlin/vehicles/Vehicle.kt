facd7e062129dbaaa2394e7c7786e6a8f31b014a
fsalcedo <fernando.salcedo@mercadolibre.com.co>
2022-05-24 15:18:51 -0500

    IMPLEMENTACION Se agrega clase Enun Class VehicleCost Se agrega el cuerpo de clase Vehicle

--- src/main/kotlin/vehicles/Vehicle.kt
+++ src/main/kotlin/vehicles/Vehicle.kt
@@ -1,3 +1,16 @@
 package vehicles
 
-class Vehicle
+import utils.VehicleCost
+
+data class Vehicle(val plate: String, val vehicleType: VehicleCost) {
+
+    override fun equals(other: Any?): Boolean {
+        if (other is Vehicle) {
+            return this.plate == other.plate
+        }
+
+        return super.equals(other)
+    }
+
+    override fun hashCode() = this.plate.hashCode()
+}
